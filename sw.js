bfc36633a13f18032cd28f628d1a109ed0046e03
ONLINE <online2023byccreaters2022@gmail.com>
2026-02-21 11:38:40 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const cacheName = "onno-cache-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        "./",
+        "./onlinenotes.html",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
